d5601ce0e97e0d9a2bf9012b4083fc2d99ec77e4
Sebastian <sebastian@Mac-mini.local>
2025-09-24 18:07:55 -0400

    Week 3

---
+++ mostWater.js
@@ -0,0 +1,21 @@
+
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxWater = 0;
+    
+    while (left < right) {
+        const width = right - left;
+        const currentHeight = Math.min(height[left], height[right]);
+        const area = width * currentHeight;
+        maxWater = Math.max(maxWater, area);
+        
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    return maxWater;
+};
