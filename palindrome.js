01677f327113473555ded48fb8d2bc76db091ebd
Sebastian Vivas <seb.vivas.dev@gmail.com>
2025-09-27 10:10:43 -0400

    Week 3

---
+++ palindrome.js
@@ -0,0 +1,30 @@
+var isPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+    
+    while (left < right) {
+        while (left < right && !isAlphanumeric(s[left])) {
+            left++;
+        }
+        
+        while (left < right && !isAlphanumeric(s[right])) {
+            right--;
+        }
+        
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+        
+        left++;
+        right--;
+    }
+    
+    return true;
+};
+
+function isAlphanumeric(char) {
+    const code = char.charCodeAt(0);
+    return (code >= 48 && code <= 57) ||  // 0-9
+           (code >= 65 && code <= 90) ||  // A-Z
+           (code >= 97 && code <= 122);   // a-z
+}
